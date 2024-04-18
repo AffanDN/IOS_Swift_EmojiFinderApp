bb486d59c81dce410cfd371dddf67986b3cdbdc6
AffanDN <affan03nusli@gmail.com>
2024-04-18 09:25:48 +0700

    Add TabView and badge

---
+++ EmojiFinder/View/Tab/MainView.swift
@@ -0,0 +1,35 @@
+//
+//  MainView.swift
+//  EmojiFinder
+//
+//  Created by Macbook Pro on 18/04/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Home", systemImage: "ev.plug.dc.nacs")
+                }
+            
+            Text("Map View")
+                .tabItem {
+                    Label("Map View", systemImage: "binoculars.fill")
+                }
+            // nampilin badge
+                .badge(4)
+            Text("Settings View")
+                .tabItem {
+                    Label("Settings View", systemImage: "gamecontroller.fill")
+                }
+            
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
